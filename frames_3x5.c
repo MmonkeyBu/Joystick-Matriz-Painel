3fac2dd6603fd158a4cac4d79e1e0a47ea8758d5
Carlos Elias <carloseliasf146@gmail.com>
2025-03-14 05:09:48 -0300

    mudanças em alguns logs
    aprimoramento de logs para leitura via software e armazenamento para análise futura

---
+++ frames_3x5.c
@@ -0,0 +1,15 @@
+#include "frames_3x5.h"
+
+double f_01_fino[NUM_LEDS_FINO] = { // completamente apagado
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0};
+
+double f_02_fino[NUM_LEDS_FINO] = { // todos acesos
+    1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0};
